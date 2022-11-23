15e6f25f51e2102ccfd4a3eb949b5082fa8b5af3
shoibaldas <shoibalshantu@gmail.com>
2022-11-24 01:00:29 +0600

    context api has been added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { getAuth } from 'firebase/auth';
+import app from '../firebase/firebaseconfig';
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    
+    const authInfo = {}
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
