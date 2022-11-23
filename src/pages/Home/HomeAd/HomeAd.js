1ebd045bb775d497a3c5135f0ec9186ac7bbf127
shoibaldas <shoibalshantu@gmail.com>
2022-11-24 00:19:05 +0600

    basic project structure initialized

---
+++ src/pages/Home/HomeAd/HomeAd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeAd = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeAd;
---
+++ src/pages/Home/HomeCategories/HomeCategories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeCategories = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomeCategories;
